60c7700c2e3ad72d3d74e55faed1659836443228
Nathaniel Rankin Webb <nrwebb@live.com>
2018-11-03 15:37:58 -0500

    Removed more of Redux Form

--- src/store.js
+++ src/store.js
@@ -1,17 +1,13 @@
-import { createStore, applyMiddleware, combineReducers, compose } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
 import promise from "redux-promise-middleware";
-import { reducer as formReducer } from "redux-form";
 import rootReducer from "./reducers";
 
 const middleware = applyMiddleware(thunk, logger);
 
-const reducers = { form: formReducer, app: rootReducer };
-
-const reducer = combineReducers(reducers);
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducer, composeEnhancers(middleware));
+const store = createStore(rootReducer, composeEnhancers(middleware));
 
 export default store;
