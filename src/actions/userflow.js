c6b8ff43a71f8ec2031d7030b4696951a19a4efb
John Keller <jmkeller3@gmail.com>
2018-11-26 23:36:51 -0700

    added userflow connection

--- src/actions/userflow.js
+++ src/actions/userflow.js
@@ -2,36 +2,44 @@ const { REACT_APP_API_BASE_URL } = process.env;
 
 // Login
 export const login = async (username, pass) => {
-  const user = {
-    username,
-    pass
-  };
+  try {
+    const user = {
+      username,
+      pass
+    };
 
-  const JWT = await fetch(`${REACT_APP_API_BASE_URL}/api/auth/login`, {
-    method: "POST",
-    mode: "cors",
-    body: user
-  });
+    const JWT = await fetch(`${REACT_APP_API_BASE_URL}/api/auth/login`, {
+      method: "POST",
+      mode: "cors",
+      body: user
+    });
 
-  return JWT;
+    return JWT;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 // Sign-Up
 // Send to server: Username, Email, Password
 // Get from server: JWT
 export const signup = async (username, email, pass) => {
-  const user = {
-    username,
-    email,
-    pass
-  };
-  const JWT = await fetch(`${REACT_APP_API_BASE_URL}/api/auth/signup`, {
-    method: "POST",
-    mode: "cors",
-    body: user
-  });
+  try {
+    const user = {
+      username,
+      email,
+      pass
+    };
+    const JWT = await fetch(`${REACT_APP_API_BASE_URL}/api/auth/signup`, {
+      method: "POST",
+      mode: "cors",
+      body: user
+    });
 
-  return JWT;
+    return JWT;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 // Search Restaurants
@@ -39,26 +47,32 @@ export const signup = async (username, email, pass) => {
 // Get from server: Restaurant Data from Yelp (name, address, distance) & Time data from server (average wait time)
 // Report time button
 export const searchRestaurants = async ({ latitude, longituge, cityState }) => {
-  // POST FETCH REQUEST
-  const url = new URL(`${REACT_APP_API_BASE_URL}/api/restaurants`);
-  let params;
-  cityState
-    ? (params = { location: cityState })
-    : (params = { latitude, longituge });
+  try {
+    // POST FETCH REQUEST
+    const url = new URL(`${REACT_APP_API_BASE_URL}/api/restaurants`);
+    let params;
+    cityState
+      ? (params = { location: cityState })
+      : (params = { latitude, longituge });
 
-  Object.keys(params).forEach((key, value) => {
-    url.searchParams.append(key, value);
-  });
-  const restaurants = await fetch(url);
+    Object.keys(params).forEach((key, value) => {
+      url.searchParams.append(key, value);
+    });
+    const restaurants = await fetch(url);
 
-  return restaurants;
+    return restaurants;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 // Report Time
 // Send to server: Restaurant id, time, JWT
 // Get from server: Send user's updated points
 export const reportTime = async (restaurantId, time, JWT) => {
-  let restaurants = JSON.parse(localStorage.getItem("restaurants"));
+  // const url = new URL(
+  //   `${REACT_APP_API_BASE_URL}/api/restaurants/${restaurantId}`
+  // );
 
   let updatedRestaurants = restaurants.map(restaurant => {
     if (restaurantId === restaurant.id) {
@@ -96,152 +110,80 @@ export const reportTime = async (restaurantId, time, JWT) => {
 // Get User Reported Times
 // Send to server: JWT
 // Get from server: User's report times (Restaurant name, time reported (id and the date), user's points)
-export const accountUser = async JWT => {
-  let users = JSON.parse(localStorage.getItem("users"));
-  let reports = JSON.parse(localStorage.getItem("reports"));
+export const accountUser = async (JWT, user_id) => {
+  try {
+    let user = await fetch(`${REACT_APP_API_BASE_URL}/api/users/${user_id}`, {
+      method: "GET",
+      headers: new Headers({
+        Authorization: `Bearer ${JWT}`,
+        "Content-Type": "application/json"
+      })
+    });
 
-  function authenication(user) {
-    return JWT === user.id;
+    let reports = user.reports.map(report => {
+      fetch(`{REACT_APP_API_BASE_URL}/api/reports/${report}`);
+    });
+
+    return {
+      reports,
+      points: user.points
+    };
+  } catch (err) {
+    console.log(err);
   }
-
-  let user = users.find(authenication());
-  if (user == null) {
-    throw Error(`Bad login request`);
-  }
-
-  const user_reports = reports.filter(report =>
-    users.reports.includes(report.id)
-  );
-
-  return {
-    reports: user_reports,
-    points: user.points
-  };
 };
 
 // Edit Reported Times
 // Send to server: JWT, report id, new time
 // Get from server: new time
 export const editTime = async (JWT, reportId, newTime) => {
-  let users = JSON.parse(localStorage.getItem("users"));
-  let reports = JSON.parse(localStorage.getItem("reports"));
+  try {
+    fetch(`${REACT_APP_API_BASE_URL}/api/reports/${reportId}`, {
+      method: "PUT",
+      headers: new Headers({
+        Authorization: `Bearer ${JWT}`,
+        "Content-Type": "application/json"
+      }),
+      body: newTime
+    });
 
-  let selectReport = reports.map(report => {
-    if (reportId === report.id) {
-      return {
-        ...report,
-        time: newTime
-      };
-    }
-    return report;
-  });
-
-  localStorage.setItem("reports", JSON.stringify(selectReport));
+    return newTime;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 // Delete Reported Times
 // Send to server: JWT, report id
 // Get from server: deletion
 export const deleteTime = async (JWT, reportId) => {
-  let users = JSON.parse(localStorage.getItem("users"));
-  let reports = JSON.parse(localStorage.getItem("reports"));
-
-  let authenication = function(user) {
-    return JWT === user.id;
-  };
-
-  let user = users.find(authenication);
-  if (user == null) {
-    throw Error(`Bad login request`);
+  try {
+    fetch(`${REACT_APP_API_BASE_URL}/api/reports/${reportId}`, {
+      method: "DELETE",
+      headers: new Headers({
+        Authorization: `Bearer ${JWT}`,
+        "Content-Type": "application/json"
+      })
+    });
+    return console.log(`Report Deleted`);
+  } catch (err) {
+    console.log(err);
   }
-
-  function findReport(report) {
-    return reportId === report.id;
-  }
-  let deletedReport = users.findIndex(findReport);
-  reports.splice(deletedReport, 1);
 };
 
 // Get Users
 export const getAllUsers = async JWT => {
-  let users = JSON.parse(localStorage.getItem("users"));
-  return users;
+  try {
+    let users = await fetch(`${REACT_APP_API_BASE_URL}/api/users`);
+
+    return users;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 // Log Out
 export const logOut = async () => {
   localStorage.clear("user");
-  localStorage.clear("newUser");
+  localStorage.clear("token");
 };
-
-// Reports Data
-// [{
-// id:
-// user_id:
-// restaurant_id:
-// time:
-// date:
-// // }]
-// Restaurant Data
-// [{
-//   id: 1,
-//   name: "Bill Bob's Burgers",
-//   address: "123 Main St. Lehi, UT 84043",
-//   distance: "1.4 mi",
-//   reported_times: [392000, 420000, 498000]
-
-// },
-// {
-//   id: 2,
-//   name: "Grace's Bakery",
-//   address: "3 Main St. Lehi, UT 84043",
-//   distance: "2.4 mi",
-//   reported_times: [465000, 594000]
-// },
-// {
-//   id: 3,
-//   name: "Stevo's Bar and Grill",
-//   address: "100 Center St. Lehi, UT 84043",
-//   distance: "0.5 mi",
-//   reported_times: [780000]
-// }];
-// // User Data
-// users: [{
-//  id: 1,
-//  username: "test",
-//  email: "test@test.com",
-//  password: "password123",
-//  points: 55,
-//  reports: [{
-//    id: 100,
-//    restaurantId: 1000,
-//    restaurantName: "Dave's Cafe",
-//    time: 525000,
-//    date: "October 2, 2018"
-// }]
-// }, {
-//  id: 2,
-//  username: "john",
-//  email: "john@john.com",
-//  password: "password123",
-//  points: 12,
-//  reports: [{
-//    id: 101,
-//    restaurantId: 1001,
-//    restaurantName: "Bill's Shop",
-//    time: 726000,
-//    date: "October 3, 2018"
-// }]
-// }, {
-//  id: 3,
-//  username: "amanda",
-//  email: 'amanda@amanda.com',
-//  password: 'password123',
-//  points: 7,
-//  reports: [{
-//    id: 102,
-//    restaurantId: 1002,
-//    time: 1128000,
-//    date: 'October 4, 2018'
-// }]
-// }]
