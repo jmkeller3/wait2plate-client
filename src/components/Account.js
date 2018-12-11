32c12e7c850cdf5ea16496df77f670b38ad1ee6b
John Keller <jmkeller3@gmail.com>
2018-12-10 19:53:59 -0700

    removed table from account to figure out loading error

--- src/components/Account.js
+++ src/components/Account.js
@@ -2,13 +2,35 @@ import React from "react";
 import { connect } from "react-redux";
 
 import "./Account.css";
-import Table from "./Table";
 
-import { accountUserThunk } from "../actions";
+
+import { accountUserThunk, editTimeThunk, deleteTimeThunk } from "../actions";
 
 export class Account extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      showModal: false
+    }
+
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this)
+  }
+
+  handleOpenModal() {
+    this.setState({ showModal: true })
+  }
+
+  handleCloseModal() {
+    this.setState({ showModal: false })
+  }
+
+  handleEdit() {
+
+  }
+
+  handleDelete() {
+
   }
 
   componentDidMount() {
@@ -18,6 +40,33 @@ export class Account extends React.Component {
 
   render() {
     console.log(this.props.reports)
+
+    function secondsToMinutesAndSeconds(seconds) {
+      const minutes = Math.floor(seconds / 60);
+      return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+    }
+    const data = this.props.reports.filter(
+      report => report.user_id === this.props.user.id
+    );
+
+    let tableData = data.map(datum => {
+      let time = secondsToMinutesAndSeconds(datum.time);
+      let date = moment(datum.date).format("MMMM D, YYYY")
+      return (
+        <tr key={Math.random()}>
+          <td>{datum.restaurant_name}</td>
+          <td>{time}</td>
+          <td>{date}</td>
+          <td>
+            <button type="button" onClick={this.handleEdit}>Edit</button>
+          </td>
+          <td>
+            <button type="button" onClick={this.handleDelete}>Delete</button>
+          </td>
+        </tr>
+      );
+    });
+
     return (
       <main role="main">
         <header role="banner">
@@ -34,27 +83,27 @@ export class Account extends React.Component {
           </header>
         </section>
 
-        {
-          this.props.reports === []
-            ? <section>
-              <header>
-                <h3>My Times</h3>
-              </header>
-              <div className="times">
-                <p>Report Times to Gain Points and Review Them Here!</p>
-              </div>
+        <section>
+          <header>
+            <h3>My Times</h3>
+          </header>
+          <div className="times">
+            {/* <Table user={this.props.user} reports={this.props.reports} /> */}
+            <table>
+              <thead>
+                <tr>
+                  <th>Restaurant</th>
+                  <th>Time</th>
+                  <th>Date</th>
+                  <th>Edit</th>
+                  <th>Delete</th>
+                </tr>
+              </thead>
+              <tbody>{tableData}</tbody>
+            </table>
+          </div>
 
-            </section> : <section>
-              <header>
-                <h3>My Times</h3>
-              </header>
-              <div className="times">
-                <Table user={this.props.user} reports={this.props.reports} />
-              </div>
-
-            </section>
-
-        }
+        </section>
       </main>
     );
   }
@@ -68,7 +117,9 @@ const mapStatetoProps = state => ({
 });
 
 const mapDispatchtoProps = {
-  accountUserThunk
+  accountUserThunk,
+  editTimeThunk,
+  deleteTimeThunk
 };
 
 export default connect(
