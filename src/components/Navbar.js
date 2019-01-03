af1bd293d5a5d2fc17f8302cd5df330ccbdd4151
John Keller <jmkeller3@gmail.com>
2019-01-03 14:19:59 -0700

    fixed logout link on mobile to take user to home screen

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -35,7 +35,8 @@ export default class Navbar extends React.Component {
   }
 
   signOut() {
-    localStorage.clear()
+    localStorage.clear();
+    this.props.history.push(`/`);
   }
 
   render() {
